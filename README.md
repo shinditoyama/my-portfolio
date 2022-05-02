51164857b655cdcc4515c3a6860bff65c000f4d5
Shindi Toyama <shinditoyama@gmail.com>
2022-05-01 22:57:14 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# My Portfolio
+
+Responsive Portfolio Website Using HTML, CSS and JavaScript.
+
+## Plugins / Tools
+<ul>
+    <li>Bootstrap</li>
+    <li>ScrollReveal</li>
+    <li>Formspree</li>
+    <li>Partcles.js</li>
+    <li>BaguetteBox.js</li>
+</ul>
+
+## Links
+<a href="https://getbootstrap.com/">https://getbootstrap.com</a>
+<a href="https://scrollrevealjs.org/">https://scrollrevealjs.org</a>
+<a href="https://formspree.io/">https://formspree.io</a>
+<a href="https://github.com/VincentGarreau/particles.js/">https://github.com/VincentGarreau/particles.js/</a>
+<a href="https://github.com/feimosi/baguetteBox.js/">https://github.com/feimosi/baguetteBox.js</a>
---
+++ assets/js/main.js
@@ -0,0 +1,70 @@
+let lastScrollPosition = 0;
+const navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', function (e) {
+    lastScrollPosition = window.scrollY;
+
+    if (lastScrollPosition > 100)
+        navbar.classList.add('navbar-dark');
+    else
+        navbar.classList.remove('navbar-dark');
+});
+
+// Activate Bootstrap scrollspy on the main nav element
+const mainNav = document.body.querySelector('#mainNav');
+if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+        target: '#mainNav',
+        offset: 74,
+    });
+};
+
+// Collapse responsive navbar when toggler is visible
+const navbarToggler = document.body.querySelector('.navbar-toggler');
+const responsiveNavItems = [].slice.call(
+    document.querySelectorAll('#navbarResponsive .nav-link')
+);
+responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener('click', () => {
+        if (window.getComputedStyle(navbarToggler).display !== 'none') {
+            navbarToggler.click();
+        }
+    });
+});
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 100 || document.documentElement.scrollTop > 100
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+});
+
+sr.reveal('#home, .about-content-left, .about-content-right, .card-box', { interval: 200 });
+sr.reveal('.contact-content-left, .contact-content-right', { interval: 200 });
+
+// Activate BaguetteBox plugin for portfolio items
+baguetteBox.run('.tz-gallery');
